614c7486ae3c2643b7af064dfa76b2f859efdd0a
Amo Chen <spitfire.sidra@gmail.com>
2015-12-13 21:20:05 +0800

    pep8 & package dependency control

--- lass-json.py
+++ lass-json.py
@@ -1,69 +1,82 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import paho.mqtt.client as mqtt
-import simplejson
 import time
 import sys
+import simplejson
 
-temp_queue = list()
+import paho.mqtt.client as mqtt
+
+
+_temp_queue = list()
+
+
+MQTT_SERVER = "gpssensor.ddns.net"
+MQTT_PORT = 1883
+MQTT_KEEPLIVE_SEC = 60
+TOPIC = "LASS/Test/PM25"
+
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, rc):
-	if rc == 0:
-		print >>sys.stderr, "NOTICE: Connected"
-		client.subscribe("LASS/Test/PM25")
+    if rc == 0:
+        print >>sys.stderr, "NOTICE: Connected"
+        client.subscribe(TOPIC)
+
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
+    # gather data from broker
+    _temp_queue.append(msg.payload)
 
-	# gather data from broker
-	temp_queue.append(msg.payload)
 
-def on_disconnect (client, userdata, msg):
-	print >>sys.stderr, "NOTICE: Disconnected"
-	client.unsubscribe("LASS/Test/PM25")
+def on_disconnect(client, userdata, msg):
+    print >>sys.stderr, "NOTICE: Disconnected"
+    client.unsubscribe(TOPIC)
 
-def queue_to_json (queue):
-	
-	ret_json = str()
-	ret_list = list()
 
-	for q in queue:
-		texts  = q.strip().split('|')
-		ret_dict = dict()
-		for text in texts:
-			if text != "":
-				name, value  = text.strip().split('=')
-				ret_dict[name] = value
-		ret_list.append(ret_dict)
+def dump_queue_to_json(queue):
 
-	# json pretty print
-	ret_json = simplejson.dumps (ret_list, sort_keys=True, indent=4 * ' ')
+    ret_list = list()
 
-	return ret_json
+    for q in queue:
 
-def main ():
-	# init 
-	client = mqtt.Client()
-	client.on_connect = on_connect
-	client.on_message = on_message
-	client.on_disconnect = on_disconnect
-	client.connect("gpssensor.ddns.net", 1883, 60)
+        ret_dict = dict()
+        texts = q.strip().split("|")
 
-	# loop for gather data
-	client.loop_start ()
+        for text in texts:
 
-	# magic wait
-	time.sleep(1)
-	print >>sys.stderr, "NOTICE: Wait 15 seconds to gather MQTT data"
-	for i in range(1, 16):
-		print >>sys.stderr, "NOTICE: " + str(i)
-		time.sleep(1)
+            if not text:
+                continue
 
-	client.loop_stop()
-	print(queue_to_json (temp_queue))
-	client.disconnect()
+            name, value = text.strip().split("=")
+            ret_dict[name] = value
 
-if __name__ == '__main__': 
-	main ()
+        ret_list.append(ret_dict)
+
+    # json pretty print
+    return simplejson.dumps(ret_list, sort_keys=True, indent=4 * " ")
+
+
+def main():
+    # init
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.on_disconnect = on_disconnect
+    client.connect(MQTT_SERVER, MQTT_PORT, MQTT_KEEPLIVE_SEC)
+
+    # loop for gather data
+    client.loop_start()
+
+    # magic wait
+    time.sleep(1)
+    print >>sys.stderr, "NOTICE: Wait 15 seconds to gather MQTT data"
+    time.sleep(15)
+
+    client.loop_stop()
+    print(dump_queue_to_json(_temp_queue))
+    client.disconnect()
+
+
+if __name__ == "__main__":
+    main()
